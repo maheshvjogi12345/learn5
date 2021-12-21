a0afd9fc637f63874ce61edfc9de004aeae4f68f
unknown <maheshvjogi.xworkz@gmail.com git config --global user.email maheshvjogi.xworkz@gmail.com>
2021-12-21 11:25:35 +0530

    lol4

---
+++ Five.java
@@ -0,0 +1,8 @@
+class Five{
+	public static void main(String args[]){
+
+		double tradingpoints[] = {1.000000002,2.000000001,3.0000000004,4.0000000003};
+		System.out.println(tradingpoints[0]);
+		System.out.println(tradingpoints[1]);
+	}
+}
